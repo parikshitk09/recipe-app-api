2699ed67e0535516f73a4e0bdcb259b8e342a020
Parikshit <parikshitk09@gmail.com>
2021-07-01 23:23:35 +0530

    Added working list tags and model

--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics,authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from user.serializers import UserSerializer,AuthTokenSerializer
@@ -11,3 +11,16 @@ class CreateTokenView(ObtainAuthToken):
     """Create new token for user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage authenticated user"""
+
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve authenticated user"""
+
+        return self.request.user
